dc242c08b00430126be05da969850e3dc3f24f0b
ronitkrshah <118371892+ronitkrshah@users.noreply.github.com>
2023-12-27 09:59:05 +0530

    Add nvim tree

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,57 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons"
+  },
+  keys = { "<leader>ee", "<leader>ef" },
+  config = function()
+
+    -- Recommended
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    -- change color for arrows in tree to light blue
+    vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#3FC5FF ]])
+    vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#3FC5FF ]])
+
+
+    -- Configuration
+    require('nvim-tree').setup({
+      hijack_cursor = true,
+      disable_netrw = true,
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+        icons = {
+          glyphs = {
+            folder = {
+              arrow_closed = "", -- arrow when folder is closed
+              arrow_open = "", -- arrow when folder is open
+            },
+          },
+        },
+      },
+      git = {
+        ignore = false,
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        icons = {
+          hint = " ",
+          info = " ",
+          warning = " ",
+          error = " ",
+        },
+      }
+    })
+
+    -- Custom Mappings
+    local keymap = vim.api.nvim_set_keymap
+
+    keymap("n", "<Leader>ee", ":NvimTreeToggle<CR>", { silent = true} )
+    keymap("n", "<Leader>ef", ":NvimTreeFindFile<CR>", { silent = true} )
+
+  end
+}

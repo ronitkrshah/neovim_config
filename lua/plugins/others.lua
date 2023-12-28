6c8989a30d85bf994369e321b90af317d0ed56ac
ronitkrshah <118371892+ronitkrshah@users.noreply.github.com>
2023-12-28 17:04:41 +0530

    Remove Filetype loading on markdownpreviewer
    There's no point to autoload this module in "markdown" files

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -16,7 +16,7 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     build = "cd app && npm install",
-    ft = "markdown",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
     end

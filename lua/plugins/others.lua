cc22be652ef2b2a580cb9211ad48a09c1bccd029
ronitkrshah <118371892+ronitkrshah@users.noreply.github.com>
2023-12-29 13:49:43 +0530

    Revert "Remove Filetype loading on markdownpreviewer"
    This reverts commit 3e7d61516dde42af6f944b599fb090ac46e4ec42.

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -16,7 +16,7 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     build = "cd app && npm install",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = "markdown",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
     end

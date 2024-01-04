a29fc4d04da8eaee7046d1cb878ab01d1b7bc2f8
ronitkrshah <118371892+ronitkrshah@users.noreply.github.com>
2024-01-04 18:08:24 +0530

    Remove <C-k> for comment
    <C-k> is already mapped for Snippet previous selection.

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,7 @@
 return {
   "numToStr/Comment.nvim",
   keys = {
-    "<C-k>", "g", "j", "i", "v", "V"
+    "g", "v", "V"
   },
   dependencies = {
     "JoosepAlviste/nvim-ts-context-commentstring",
@@ -16,15 +16,6 @@ return {
       -- for commenting tsx and jsx files
       pre_hook = ts_context_commentstring.create_pre_hook(),
     })
-
-    -- Set Custom Keymap For Faster Commenting
-    local keymap = vim.api.nvim_set_keymap
-    local opts = { silent = true }
-
-    -- CTRL + k to comment
-    keymap("n", "<C-k>", "gcc", opts)
-    keymap("i", "<C-k>", "<ESC>gcci", opts)
-    keymap("v", "<C-k>", "gc", opts)
 
   end,
 }

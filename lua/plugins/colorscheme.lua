261cda485a08d3b69f3faf14d851b8025f43a6d7
RKS <118371892+ronitkrshah@users.noreply.github.com>
2024-02-21 23:34:28 +0530

    Use nordic colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,11 @@
 return {
-  "sainnhe/gruvbox-material",
+  'AlexvZyl/nordic.nvim',
   lazy = false,
   priority = 1000,
   config = function()
+    require('nordic').setup({})
 
-    -- Configurations
-    vim.g.gruvbox_material_background = 'soft'
-    vim.g.gruvbox_material_better_performance = 1
-    vim.g.gruvbox_material_enable_bold = 1
-    vim.g.gruvbox_material_enable_italic = 1
-    vim.g.gruvbox_material_diagnostic_line_highlight = 1
-    vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
-
-    -- Apply
-    vim.cmd('colorscheme gruvbox-material')
+    -- Apply colorscheme
+    vim.cmd.colorscheme("nordic")
   end
 }

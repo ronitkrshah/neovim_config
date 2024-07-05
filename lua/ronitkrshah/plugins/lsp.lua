67fc4bf5d4ece64dd3669a9f2f418dead27a1ac2
RKS <ronitkrshah@tuta.io>
2024-07-05 17:08:00 +0530

    add: borders on hover

--- lua/ronitkrshah/plugins/lsp.lua
+++ lua/ronitkrshah/plugins/lsp.lua
@@ -81,5 +81,12 @@ return {
         })
     })
 
+    -- show borders on hover
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+      vim.lsp.handlers.hover, {
+        border = "single",
+        title = " Hover "
+      }
+    )
   end
 }

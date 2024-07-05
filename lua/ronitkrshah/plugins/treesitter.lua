365b267dffeb8d0da12bad688343d70d060f963d
RKS <ronitkrshah@tuta.io>
2024-07-05 20:04:50 +0530

    add: treesitter

---
+++ lua/ronitkrshah/plugins/treesitter.lua
@@ -0,0 +1,30 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = { "hiphish/rainbow-delimiters.nvim" },
+  build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+
+    treesitter.setup({
+      -- Required parsers
+      ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "javascript",
+        "typescript",
+        "tsx"
+      },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false
+      },
+      indent = {
+        enable = true
+      },
+    })
+  end
+}

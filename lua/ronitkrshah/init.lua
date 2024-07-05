d5ce13a8800f148ba7f94a17c18a240bfb05dcda
RKS <ronitkrshah@tuta.io>
2024-07-05 16:33:49 +0530

    add: lsp

--- lua/ronitkrshah/init.lua
+++ lua/ronitkrshah/init.lua
@@ -1,5 +1,6 @@
-require("ronitkrshah.core.keymap")
 require("ronitkrshah.core.options")
+require("ronitkrshah.core.keymap")
+require("ronitkrshah.core.lsp_keymap")
 
 -- lazy plugin manager
 require("ronitkrshah.lazy_pm")
---
+++ lua/ronitkrshah/plugins/lsp.lua
@@ -0,0 +1,85 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    -- lsp plugin manager
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+
+    -- completion
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+
+    -- snippets
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip"
+  },
+  lazy = false,
+  config = function()
+    local cmp = require("cmp")
+    local capabilities = vim.tbl_deep_extend("force",
+      {
+        workspace = {
+          didChangeWatchedFiles = {
+            dynamicRegistration = false,
+          },
+        },
+      },
+      vim.lsp.protocol.make_client_capabilities(),
+      require('cmp_nvim_lsp').default_capabilities()
+    )
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      handlers = {
+        function (server)
+          require("lspconfig")[server].setup({
+            capabilities = capabilities
+          })
+        end,
+
+        ["lua_ls"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.lua_ls.setup {
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = {"vim"},
+                }
+              }
+            }
+          }
+        end,
+      }
+    })
+
+    -- completion setup
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+      }, {
+        { name = 'buffer' },
+        })
+    })
+
+  end
+}

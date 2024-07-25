afef8fe83131263f53738c25c2b455a13a33194a
RKS <ronitkrshah@tuta.io>
2024-07-25 21:28:20 +0530

    Show flutter dev log in bottom

--- lua/ronitkrshah/plugins/flutter.lua
+++ lua/ronitkrshah/plugins/flutter.lua
@@ -5,5 +5,13 @@ return {
     'stevearc/dressing.nvim',
   },
   ft = { "dart" },
-  config = true,
+  config = function()
+    require("flutter-tools").setup({
+      dev_log = {
+        enabled = true,
+        notify_errors = false,       -- if there is an error whilst running then notify the user
+        open_cmd = "botright split", -- command to use to open the log buffer
+      },
+    })
+  end
 }
